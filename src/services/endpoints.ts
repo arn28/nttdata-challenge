372df1254f05ef87824839bbe2eb9e70788b6c18
arn28 <arnold.dev.me@gmail.com>
2024-08-15 17:46:12 -0500

    project init

---
+++ src/services/endpoints.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const ENDPOINTS = {
+  SWAPI: '',
+}
+
+export const getPeople = async (page: number = 1) => {
+  const response = await axios.get(`${ENDPOINTS.SWAPI}people/?page=${page}`)
+  return response
+}
+
+export const getOnePeople = async (id: number) => {
+  const response = await axios.get(`${ENDPOINTS.SWAPI}people/${id}`)
+  return response
+}
+
+export const getOnePlanet = async (id: number) => {
+  const response = await axios.get(`${ENDPOINTS.SWAPI}planets/${id}`)
+  return response
+}
+
+export const getVehicle = async (id: number) => {
+  const response = await axios.get(`${ENDPOINTS.SWAPI}vehicles/${id}`)
+  return response
+}
